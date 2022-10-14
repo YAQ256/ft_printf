23ed7fb1474d7aad21848bac4d571fa369cd21a7
Christian Yacoub Obage <cyacoub-@c2r9s3.42madrid.com>
2022-10-14 17:45:37 +0200

    .

--- Libft/ft_putstr.c
+++ Libft/ft_putstr.c
@@ -6,12 +6,19 @@
 /*   By: cyacoub- <cyacoub-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 15:42:49 by cyacoub-          #+#    #+#             */
-/*   Updated: 2022/10/14 15:43:04 by cyacoub-         ###   ########.fr       */
+/*   Updated: 2022/10/14 17:09:12 by cyacoub-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * It writes a string to the standard output
+ * 
+ * @param s The string to print.
+ * 
+ * @return The number of characters printed.
+ */
 int	ft_putstr(char *s)
 {
 	int	i;
